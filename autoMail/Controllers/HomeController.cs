96be9b04b5ba4295a09687c898e1af30d2ddfc4d
Gurpreetsinghsr <39495450+Gurpreetsinghsr@users.noreply.github.com>
2021-04-15 04:43:15 +0530

    Add project files.

---
+++ autoMail/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using autoMail.Models;
+using autoMail.Service;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace autoMail.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        private readonly IEmailService _emailService;
+
+        public HomeController(IEmailService emailService)
+        {
+            _emailService = emailService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            UserEmailOptions options = new UserEmailOptions
+            {
+                ToEmails = new List<string>() { "test@gmail.com" }
+            };
+            await _emailService.SendTestEmail(options);
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ autoMail/Service/IEmailService.cs
@@ -0,0 +1,10 @@
+﻿using autoMail.Models;
+using System.Threading.Tasks;
+
+namespace autoMail.Service
+{
+    public interface IEmailService
+    {
+        Task SendTestEmail(UserEmailOptions userEmailOptions);
+    }
+}
